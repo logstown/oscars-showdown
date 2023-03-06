ac8f0797d8b776d523a81c34c16190b91e21f9e4
Logan Joecks <ljoecks@gmail.com>
2023-03-06 15:42:51 -0500

    title and favicon plus other

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxc3uph6EmJgTzWtaqAo8tXjJ90MqT7rg",
+  authDomain: "oscars.firebaseapp.com",
+  databaseURL: "https://oscars.firebaseio.com",
+  projectId: "firebase-oscars",
+  storageBucket: "firebase-oscars.appspot.com",
+  messagingSenderId: "560662746392",
+  appId: "1:560662746392:web:934b530e0972216e53ba6d"
+}
